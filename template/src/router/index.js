def09016b143c1e796b26c7a4b5f81bf6e4c3c71
johnny <johnny@datwork.cn>
2019-12-20 17:51:33 +0800

    add  libs there and vue-resize-directive

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ThreePage from "@/components/ThreePage";
 import HelloWorld from '@/components/HelloWorld'
-
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'ThreePage',
+      component: ThreePage
+    },{
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
     }
